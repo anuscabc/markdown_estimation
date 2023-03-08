94f32bdff618e65e60f731eb64178f79ed6ef107
anuscabc <popoviciana1012@gmail.com>
2023-03-08 19:48:38 +0100

    Last iteration generalized Betrand

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Markdown and Markup Decomposition
 
 This repository contains the code for markdown and markup decompition. This was performed for the MPhil thesis economics at the Tinbergen Institute.
+
+I fucking hate my life 
--- src/firm.py
+++ src/firm.py
@@ -1,63 +1,47 @@
-from numpy import array
 from scipy import optimize
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-# class Firm:
+# class Firm: 
 #     def __init__(self, id, shares, cost):
         
 #         self.id = id
 #         self.shares = shares
 #         self.cost = cost 
 
-# c_s = 1
-# c_t = 3
-
-# q_s = 100 - 5*(p_s - p_t)
-# q_t = 100 - 5*(p_t - p_s)
-
-# profit_s = (p_s - c_s)*100 - 5*(p_s - p_t)
-# proft_t = (p_t - c_t)*q_t
+def demand(x_own, x_rest, b):
+    return 100 - b * (x_own - np.sum(x_rest))
 
 
-# def func(p, c, p_c):
-#    return ((p - c)*(100 - 5*(p - p_c)))
+def profit(x_own, x_rest, c_own, b):
+    return demand(x_own, x_rest, b) * (x_own - c_own)
 
-# p = np.array[]
-
-# c1 = np.array([10,12.])
-# c2 = np.array([3, 5.])
-# optimize.fixed_point(func, [1.2, 1.3], args=(c1,c2))
-# array([ 1.4920333 ,  1.37228132
+def neg_profit(x_own, x_rest, c_own, b):
+    return -1 * profit(x_own, x_rest, c_own, b)
 
 
-def demand(x1,x2,x3,b):
-    return 100- b*(x1 - x2- x3)
+def reaction(x, c, b, i):
+    c_own = c[i]
+    x_rest = np.delete(x, i)
+    params = (x_rest, c_own, b)
+    x_opt = optimize.brute(neg_profit, ((0, 1,),), args=params)
+    return x_opt[0]
 
 
-
-def profit(x1,x2, x3,c1,b):
-    return demand(x1,x2,x3, b)*(x1 - c1)
-
-
-
-def reaction(x3, x2,c1,b):
-    x1 = optimize.brute(lambda x: -profit(x,x3, x2,c1,b), ((0,1,),)) # brute minimizes the function;
-                                                                 # when we minimize -profits, we maximize profits
-    return x1[0]
+def vector_reaction(x, params): 
+    b, c, n_firms = params
+    return np.array(x) - np.array([reaction(x, c, b, i) for i in range(n_firms)])
 
 
-def vector_reaction(x,param): # vector param = (b,c1,c2)
-    return array(x)-array([reaction(x[2], x[1],param[1],param[0]),reaction(x[0], x[2],param[2],param[0]), 
-                           reaction(x[0], x[1],param[3],param[0])])
+np.random.seed(1012)
 
 
-param = [5, 1, 3, 2]
-# initial guess x0
-x0 = [1, 1, 1]
-
-ans = optimize.fsolve(vector_reaction, x0, args = (param))
-print(ans)
-
-
+b0 = 5.
+for n_firms in range(1, 10, 1):
+    c = np.random.uniform(1,4,n_firms)
+    params = [b0, c, n_firms]
+    x0 = np.ones(n_firms)
+    ans = optimize.fsolve(vector_reaction, x0, args=(params))
+    print(ans)
 
