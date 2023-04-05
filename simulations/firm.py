ca3454645b8a8a4bedb76f9ee3d3a6c9896f1e90
anuscabc <popoviciana1012@gmail.com>
2023-04-05 11:23:46 +0200

    Reset to the working market equilibrium

--- simulations/firm.py
+++ simulations/firm.py
@@ -6,37 +6,13 @@ import scipy.stats
 from scipy.optimize import minimize
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
-import autograd.numpy as np
-from autograd import grad, jacobian
-# importing the data simulation functions 
-# import demand_data_simulation
-# import clean_data
-# import demand_data_estimation
-import statsmodels.api as sm
-import statsmodels.formula.api as smf# from main import beta, mu, omega, df
-
-# This can be the share data as defined in the simulation part i think 
-np.random.seed(4)
-# def exponeential utility: 
-# This should work for normalized consumer mass to 1 
-
-# Trying to generalize the number of firms
-N = 1
-p = np.ones(N)
-c = np.random.lognormal(0, 1, size = N)
-X = np.random.lognormal(0, 1, size = N)
 
 
-# initiate some number of consumers 
 
 
-e = np.random.gumbel(0, 1, size = N)
-beta = [1., 2.]
-alpha = -0.5
 
 
 def sum_utility(beta, X, price, alpha):
-    # here think a bit about how you want to introduce the outside good 
     sum_u_exp = sum(np.exp(beta[0] + alpha*price + beta[1]*X))
     return sum_u_exp
 
@@ -46,20 +22,24 @@ def probability(beta, X, price, alpha):
 
 def profit(price, beta, X,  alpha, c):
     q = probability(beta, X, price, alpha)
-    Pi = - 1*q*(price - c)
+    Pi = q*(price - c)
     return Pi
 
-# def own_elasticites(beta, X, price, alpha):
-#     share = probability(beta, X, price, alpha)
-#     elasticity_own = alpha*share - alpha*share**2
-#     return elasticity_own
+def markup(price, c): 
+    markup = (price - c)/c
+    return markup
 
-# def cross_elasticities(beta, X, price, alpha):
-#     share = probability(beta, X, price, alpha)
-#     elasticity_cross= -alpha*share*share
-#     return elasticity_cross
 
-# Lets see if possible to get the same Jacobian like this as 
+def own_elasticites(beta, X, price, alpha):
+    share = probability(beta, X, price, alpha)
+    elasticity_own = alpha*share - alpha*share**2
+    return elasticity_own
+
+def cross_elasticities(beta, X, price, alpha):
+    share = probability(beta, X, price, alpha)
+    elasticity_cross= -alpha*share*share
+    return elasticity_cross
+
 
 def derivative(N, beta, X, price, alpha):
     share = probability(beta, X, price, alpha)
@@ -68,31 +48,17 @@ def derivative(N, beta, X, price, alpha):
         for j in range(J.shape[1]):
             if i == j: 
                 J[i, j] = alpha*share[i] - alpha*share[i]**2
-            if i!= j: 
+            elif i!= j: 
                 J[i, j] = -alpha*share[i]*share[j]
     return J
 
 
-
-def root_objective(price, N, beta, X, alpha):
+def root_objective(price, N, beta, X, alpha, c):
     share = probability(beta, X, price, alpha)
     J = derivative(N, beta, X, price, alpha) 
     optim = np.matmul(np.transpose(J), (price - c)) + share
     return optim
 
 
-# res = minimize(profit, p, args= (beta, X, alpha, c), method = "Nelder-Mead")
-# print(res.x)
 
-
-# initial optimization with 2 firms 
-
-res1 = scipy.optimize.root(root_objective, p, args=(N, beta, X, alpha), method = 'hybr')
-
-optimal_price =
-
-
-deriv = derivative(N, beta,X, p, alpha)
-sum_q = sum_utility(beta, X, p, alpha)
-prof = profit(p, beta, X, alpha, c)
 
---
+++ simulations/multiple_firms_equil.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.stats
+import matplotlib.pyplot as plt
+# importing the data simulation functions 
+# import demand_data_simulation
+# import clean_data
+# import demand_data_estimation
+import firm
+
+# This can be the share data as defined in the simulation part i think 
+np.random.seed(7)
+# def exponeential utility: 
+# This should work for normalized consumer mass to 1 
+
+# define the global parameter and the global number of firms 
+N = 10
+
+# put in the true param because the firm actually knows them
+beta = [2., -0.611]
+alpha = -2.71
+
+
+average_price = []
+average_profit = []
+average_markup = []
+
+
+for n in range(1, N+1, 1):
+    p = np.ones(n)
+    c = np.random.lognormal(0, 1, size = n)
+    X = np.random.lognormal(0, 1, size = n)
+    e = np.random.gumbel(0, 1, size = n)
+
+    # getting all the values of interest at the optimal price
+    res1 = scipy.optimize.root(firm.root_objective, p, args=(n, beta, X, alpha, c), method = 'hybr')
+    optimal_price = res1.x
+    profit = firm.profit(optimal_price, beta, X, alpha, c)
+    markup = firm.markup(optimal_price, c)
+
+    # putting all the means in a list to append later 
+    mean_optimal_price = np.mean(optimal_price)
+    mean_optimal_profit = np.mean(profit)
+    mean_optimal_markup = np.mean(markup)
+    average_price.append(mean_optimal_price)
+    average_profit.append(mean_optimal_profit)
+    average_markup.append(mean_optimal_markup)
+
+print(average_price)
+print(average_profit)
+print(average_markup)
+
+x_axis = range(1, N+1, 1)
+# plt.plot(x_axis, average_price)
+# plt.title('Average market price equilibirum')
+# plt.xlabel('Number of firms')
+# plt.ylabel('Price')
+# plt.savefig('plots/average_price.png')
+
+
+# plot_profit =  plt.plot(x_axis, average_profit)
+# plt.title('Average market profit equilibirum')
+# plt.xlabel('Number of firms')
+# plt.ylabel('Price')
+# plt.savefig('plots/average_profit.png')
+
+
+# plot_mark_up =  plt.plot(x_axis, average_mark_up)
+# plt.title('Average market markup equilibirum')
+# plt.xlabel('Number of firms')
+# plt.ylabel('Price')
+# plt.savefig('plots/average_markup.png')
+
+fig, axs = plt.subplots(3)
+fig.suptitle('Market Equilibium Over Different Number of Frims')
+axs[0].plot(x_axis, average_price, 'tab:orange')
+axs[0].set_title("Price")
+axs[1].plot(x_axis, average_profit, 'tab:green')
+axs[1].set_title("Profit")
+axs[2].plot(x_axis, average_markup)
+axs[2].set_title("Markup")
+fig.tight_layout()
+fig.savefig("plots/all.png")
+
