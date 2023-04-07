c4b8b6143342eccd74efb6fe604b5eac5f5c29fc
anuscabc <popoviciana1012@gmail.com>
2023-04-07 23:47:36 +0200

    stabilitzation equilibrium

--- simulations/multiple_firms_equil.py
+++ simulations/multiple_firms_equil.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 import firm
 
 # This can be the share data as defined in the simulation part i think 
-np.random.seed(7)
+seed = 1012
+np.random.seed(seed)
 # def exponeential utility: 
 # This should work for normalized consumer mass to 1 
 
 # The number of firms in the market
-N = 10
+N = 100
 
 # The number of product characteristics
 J = 2
@@ -26,43 +27,46 @@ beta = np.array([2., -0.611, -0.528])
 alpha = -2.71
 
 average_price = []
-# average_profit = []
-# average_markup = []
+average_profit = []
+average_markup = []
+outside_good_share = []
+
+# Talk about the stochasticity when there are too large variation
+all_Xj = np.random.uniform(4, 5, size=(N, J))
+all_c = np.random.uniform(1, 2, size=N)
 
 for n in range(1, N+1, 1):
     p = np.ones(n)
-    Xj = np.random.lognormal(0, 1, size = (n, J)) 
+    # Xj = np.random.lognormal(0, 1, size = (n, J)) 
+    Xj = all_Xj[:n, :]
     X0 = np.ones(n)
     X = np.column_stack((X0, Xj))
-    c = np.random.lognormal(0, 1, size = n)
+    # c = np.random.lognormal(0, 1, size = n)
+    c = all_c[:n]
+
     # getting all the values of interest at the optimal price
-    res1 = scipy.optimize.root(firm.root_objective, p, args=(n, X, beta, alpha, c), method = 'broyden2')
+    res1 = scipy.optimize.root(firm.root_objective, p, args=(n, X, beta, alpha, c),
+                               method='broyden2')
     optimal_price = res1.x
-    # markup = firm.markup(optimal_price, c)
-    # share = firm.probability(optimal_price, alpha, X, beta)
-    # profit = firm.profit(optimal_price, share, c)
+    markup = firm.markup(optimal_price, c)
+    share = firm.probability(optimal_price, alpha, X, beta)
+    outside_good = 1 - sum(share)
+    profit = firm.profit(optimal_price, share, c)
+
     # putting all the means in a list to append later 
     mean_optimal_price = np.mean(optimal_price)
-    # mean_optimal_profit = np.mean(profit)
-    # mean_optimal_markup = np.mean(markup)
+    mean_optimal_profit = np.mean(profit)
+    mean_optimal_markup = np.mean(markup)
     average_price.append(mean_optimal_price)
-    # average_markup.append(mean_optimal_markup)
-    # average_profit.append(mean_optimal_profit)
+    average_markup.append(mean_optimal_markup)
+    average_profit.append(mean_optimal_profit)
+    outside_good_share.append(outside_good)
 
-print(average_price)
-# print(average_profit)
-# print(average_markup)
+df = pd.DataFrame({'avg_price': average_price,
+                   'avg_profit': average_profit,
+                   'avg_markup': average_markup,
+                   'outside_share': outside_good_share})
+df.to_csv(f'data/sim_{seed}.csv', index=False)
 
-x_axis = range(1, N+1, 1)
 
-# fig, axs = plt.subplots(3)
-# fig.suptitle('Market Equilibium Over Different Number of Frims')
-# axs[0].plot(x_axis, average_price, 'tab:orange')
-# axs[0].set_title("Price")
-# axs[1].plot(x_axis, average_profit, 'tab:green')
-# axs[1].set_title("Profit")
-# axs[2].plot(x_axis, average_markup)
-# axs[2].set_title("Markup")
-# fig.tight_layout()
-# fig.savefig("plots/all.png")
 
