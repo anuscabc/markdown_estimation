4a689b8d9fe44a50434d2c630aea0e86c2dd68c4
anuscabc <popoviciana1012@gmail.com>
2023-04-17 14:38:17 +0200

    Full dataframe ready to be used to approximate market shares

--- simulations/generalization.py
+++ simulations/generalization.py
@@ -8,7 +8,7 @@ from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import firm_revised
 
-seed = 50
+seed = 100
 np.random.seed(seed)
 # Trying to see how sensitive computed market shares are to the initialized parameters
 # All the parameters of interest 
@@ -28,10 +28,10 @@ K = 2
 N = 500
 
 # The number of period that I want to run the market for 
-T = 10
+T = 100
 
 # The product characteritics
-X1 = np.random.uniform(3, 8, size=J)
+X1 = np.random.uniform(5, 6, size=J)
 X2 = np.random.normal(3, 1, size=J)
 all_X = np.column_stack((X1, X2))
 X0 = np.ones(J)
@@ -39,32 +39,32 @@ X = np.column_stack((X0, all_X))
 
 
 # The initialized vector of prices 
-p = np.ones(J)
+max_budget = 10
+p = np.zeros(J)
 
 
 # Need somewhere to store the costs and the prices 
 list_prices = []
 list_cost = []
+list_shares = []
+list_markup = []
+list_profit = []
 
-
-
-X1 = np.random.uniform(3, 4, size=J)
-X2 = np.random.normal(3, 1, size=J)
-all_X = np.column_stack((X1, X2))
-X0 = np.ones(J)
-X = np.column_stack((X0, all_X))
+# Get mean prices and costs
+mean_prices = []
+mean_cost = [] 
 
 
 # All of this here is for the integration of the cost 
-a = 0.8
-b = -0.0009
+a = 0.7
+b = -0.009
 
-c_max_scale = 4.
+c_max_scale = 2.
 c_min_scale = 1.
 
 C = np.zeros((J, T))
 for t in range(1, T):
-    C[:, t] = b * t + a * C[:, t-1] + np.random.normal(0, 1, size=J)
+    C[:, t] = b * t + a * C[:, t-1] + np.random.normal(0, 0.5, size=J)
 c_max = np.tile(np.array([np.amax(C, axis=1)]).T, (1, T))
 c_min = np.tile(np.array([np.amin(C, axis=1)]).T, (1, T))
 
@@ -77,21 +77,64 @@ for t in range (1, T):
     v_p = np.random.normal(0, 1, size = N)
     e = np.random.gumbel(size=N*J)
     c = C[:, t]
-    res1 = scipy.optimize.root(firm_revised.root_objective, p, 
-                               args=(N, J, X, v_p, beta, mu, omega, e, c), method='broyden2')
+    res1 = scipy.optimize.root(firm_revised.root_objective, p, args=(N, J, X, v_p, beta, mu, omega, e, c),
+                            method='broyden2')
     optimal_price = res1.x
-    list_prices.append(optimal_price[1])
-    list_cost.append(c[1])
+    for i in range(0, J):
+        if optimal_price[i] < c[i]:
+            optimal_price[i] = c[i]
+    for x in range(0, J): 
+        if optimal_price[x]> max_budget:
+            optimal_price[x] = max_budget   
+    shares = firm_revised.share(N, J, X, v_p, optimal_price, beta, mu, omega, e)
+    profits = firm_revised.profit(optimal_price, shares, c)
+    markups = firm_revised.markup(optimal_price, c)
+
+    print(1-sum(shares))
+
+
+    list_prices.append(optimal_price)
+    list_cost.append(c)
+    list_shares.append(shares)
+    list_profit.append(profits)
+    list_markup.append(markups)
+
 
 
 # Integrate the product characteristics from the other market 
-print(list_prices)
-print(list_cost)
+prices1 = np.array(list_prices).flatten()
+cost1 = np.array(list_cost).flatten()
+shares1 = np.array(list_shares).flatten()
+profits1 = np.array(list_profit).flatten()
+markups1 = np.array(list_markup).flatten()
+products = np.tile(np.array(range(1, J+1)), T-1)
+time = np.repeat(np.array(range(1, T)), J)
+Car1 = np.tile(X1, T-1)
+Car2 =np.tile(X2, T-1)
+
+print(f"This is the mean markup: {np.mean(markups1)}")
+print(f"This is the mean prifits: {np.mean(profits1)}")
 
 
+print(prices1.shape)
+print(cost1.shape)
+print(time.shape)
+print(Car1.shape)
+print(Car2.shape)
+print(shares1.shape)
+print(profits1.shape)
+print(markups1.shape)
 
-# df = pd.DataFrame({'price': list_prices,
-#                    'cost': list_cost})
-# df.to_csv(f'data/individual_{seed}.csv', index=False)
+df = pd.DataFrame({'price': prices1,
+                   'cost': cost1,
+                   'product': products, 
+                   'time':time, 
+                   'Car1':Car1, 
+                   'Car2':Car2,
+                   'mshare':shares1, 
+                   'profits':profits1, 
+                   'markups': markups1})
+df.to_csv(f'data/market_{seed}.csv', index=False)
+print(df)
 
 
---
+++ simulations/monte_carlo.py
@@ -0,0 +1,1 @@
+# Monte Carlo simulation for the approximation og the market shares 
