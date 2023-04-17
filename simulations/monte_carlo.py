c920e8432ee546f16a5857124a01bef05379703d
anuscabc <popoviciana1012@gmail.com>
2023-04-17 15:44:59 +0200

    Slicing eq shares and prices for the MC bug fix

--- simulations/monte_carlo.py
+++ simulations/monte_carlo.py
@@ -1,1 +1,25 @@
 # Monte Carlo simulation for the approximation og the market shares 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import firm_revised
+# Get in the dataframe that you want and then unpack everything from it for the monte carlo 
+
+
+# Need to put all the parametrs in the same vextor 
+def f(theta, df_MC, df):
+    shares_true = df['shares'].to_numpy()
+    shares_est, _ = firm_revised.share()
+    return np.linalg.norm(shares_true - shares_est.to_numpy())
+
+res = minimize(f, theta_0, args=(df_MC, df), method = 'Nelder-Mead')
+print(res.x)
+print(theta_true)
+
+end = time.time()
+
+print("The time of execution of above program is :",
+      (end-start) /60, "minutes")
+ 
